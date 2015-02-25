e2a3893237a1dd385c22e5c66540c3227e196b6e
Joe Drago <jdrago@lgmd-jdrago.corp.netflix.com>
2015-02-25 15:06:38 -0800

    messing with mouse events (scale seems wrong)

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,96 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+function eventToMousePos(event, elem) {
+    if (elem == undefined){
+        elem = document.body;
+    }
+
+    var isTouch = false;
+    var pos = {
+        x: 0,
+        y: 0
+    };
+
+    // touch screen events
+    if (event.touches){
+        if (event.touches.length){
+            isTouch = true;
+            pos.x = parseInt(event.touches[0].pageX);
+            pos.y = parseInt(event.touches[0].pageY);
+        }
+    }else{
+        // mouse events
+        pos.x = parseInt(event.clientX);
+        pos.y = parseInt(event.clientY);
+    }
+
+    // accounts for border
+    pos.x -= elem.clientLeft;
+    pos.y -= elem.clientTop;
+
+//    // parent offsets
+//    var par = elem;
+//    while (par !== null) {
+//        if (isTouch){
+//            // touch events offset scrolling with pageX/Y
+//            // so scroll offset not needed for them
+//            pos.x -= parseInt(par.offsetLeft);
+//            pos.y -= parseInt(par.offsetTop);
+//        }else{
+//            pos.x += parseInt(par.scrollLeft - par.offsetLeft);
+//            pos.y += parseInt(par.scrollTop - par.offsetTop);
+//        }
+//
+//        par = par.offsetParent || null;
+//    }
+
+    //Mouse.x /= Stage.scaleFactor;
+    //Mouse.y /= Stage.scaleFactor;
+
+    return pos;
+}
+
+
+function debug() {
+    console.log("DEBUG FUNCTION");
+
+    var canvas = FastCanvas.create(); // specific to FastCanvas
+    var context = canvas.getContext("2d");
+    var myImage = FastCanvas.createImage(); // specific to FastCanvas
+    myImage.onload = function(){
+           context.setTransform(1, 0, 0, 1, 0, 0);
+           context.translate(200, 200);
+           context.rotate(Math.PI);
+           context.drawImage(myImage, 0, 0);
+           FastCanvas.render(); // specific to FastCanvas
+    }
+    myImage.src = "img/logo.png";
+
+    var isTouchSupported = "ontouchstart" in window;
+    var startEventName = isTouchSupported ? "touchstart" : "mousedown";
+    var moveEventName = isTouchSupported ? "touchmove" : "mousemove";
+    var endEventName = isTouchSupported ? "touchend" : "mouseup";
+
+    document.addEventListener(moveEventName, function(evt, elem) {
+        evt.preventDefault()
+        var pos = eventToMousePos(evt, elem);
+        //return if window.pointerId != null
+        //window.pointerId = evt.pointerId
+        console.log("move: " + pos.x + ", " + pos.y);
+        console.log(evt);
+    }, false);
+
+//$('#screenCanvas').on 'pointermove', (evt) ->
+//evt.preventDefault()
+//return if window.pointerId != evt.pointerId
+//window.engine.mouseMove Math.floor(evt.clientX / window.scale), Math.floor(evt.clientY / window.scale)
+//
+//$('#screenCanvas').on 'pointerup', (evt) ->
+//evt.preventDefault()
+//return if window.pointerId != evt.pointerId
+//window.pointerId = null
+//window.engine.mouseUp Math.floor(evt.clientX / window.scale), Math.floor(evt.clientY / window.scale)
+
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,7 +108,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
+        debug();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
