9e97a658e92ba2b5ec675474fb94896582a1e3a4
Joe Drago <jdrago@lgmd-jdrago.corp.netflix.com>
2015-02-25 10:40:18 -0800

    FastCanvas support

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -2,6 +2,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.adobe.plugins.FastCanvas": "1.0.0"
+}
 // BOTTOM OF METADATA
 });
