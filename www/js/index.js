78b3e5544a798bb8706b97b6d42d32bf06f9b53a
Joe Drago <joedrago@gmail.com>
2015-02-25 21:49:43 -0800

    scaling works, desktop browser works

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,14 @@
+var gPaused = false;
+var thingImage;
+var thingX = 100;
+var thingY = 100;
+var gCanvas, gContext;
+var gWidth, gHeight;
+
 function eventToMousePos(event, elem) {
     if (elem == undefined){
         elem = document.body;
     }
-
-    console.log("ELEMENT SIZE: ", elem);
 
     var isTouch = false;
     var pos = {
@@ -28,50 +33,32 @@ function eventToMousePos(event, elem) {
     pos.x -= elem.clientLeft;
     pos.y -= elem.clientTop;
 
-//    // parent offsets
-//    var par = elem;
-//    while (par !== null) {
-//        if (isTouch){
-//            // touch events offset scrolling with pageX/Y
-//            // so scroll offset not needed for them
-//            pos.x -= parseInt(par.offsetLeft);
-//            pos.y -= parseInt(par.offsetTop);
-//        }else{
-//            pos.x += parseInt(par.scrollLeft - par.offsetLeft);
-//            pos.y += parseInt(par.scrollTop - par.offsetTop);
-//        }
-//
-//        par = par.offsetParent || null;
-//    }
-
-    //Mouse.x /= Stage.scaleFactor;
-    //Mouse.y /= Stage.scaleFactor;
-
     return pos;
 }
 
+function initGraphics() {
+    gWidth = window.innerWidth;
+    gHeight = window.innerHeight;
+    console.log("initGraphics(): window size " + window.innerWidth + ", " + window.innerHeight + "," + window.devicePixelRatio);
+
+    gCanvas = FastCanvas.create(!window.cordova);
+    gContext = gCanvas.getContext("2d");
+    thingImage = FastCanvas.createImage(); // specific to FastCanvas
+    thingImage.onload = function(){
+        requestAnimationFrame(render);
+    }
+    thingImage.src = "img/logo.png";
+}
 
 function debug() {
     console.log("DEBUG FUNCTION");
-    console.log("window size " + window.innerWidth + ", " + window.innerHeight + "," + window.devicePixelRatio);
+    console.log("debug(): window size " + window.innerWidth + ", " + window.innerHeight + "," + window.devicePixelRatio);
 
     var mvp = document.getElementById('gameViewport');
     var scale = 1 / window.devicePixelRatio;
     var viewportSettings = "user-scalable=no, initial-scale="+scale+", maximum-scale="+scale+", minimum-scale="+scale+", width=device-width, height=device-height";
     console.log("viewportSettings: " + viewportSettings);
     mvp.setAttribute('content', viewportSettings);
-
-    var canvas = FastCanvas.create(); // specific to FastCanvas
-    var context = canvas.getContext("2d");
-    var myImage = FastCanvas.createImage(); // specific to FastCanvas
-    myImage.onload = function(){
-           context.setTransform(1, 0, 0, 1, 0, 0);
-           context.translate(660, 340);
-           context.rotate(Math.PI);
-           context.drawImage(myImage, 0, 0);
-           FastCanvas.render(); // specific to FastCanvas
-    }
-    myImage.src = "img/logo.png";
 
     var isTouchSupported = "ontouchstart" in window;
     var startEventName = isTouchSupported ? "touchstart" : "mousedown";
@@ -81,23 +68,44 @@ function debug() {
     document.addEventListener(moveEventName, function(evt, elem) {
         evt.preventDefault()
         var pos = eventToMousePos(evt, elem);
-        //return if window.pointerId != null
-        //window.pointerId = evt.pointerId
-        console.log("move: " + pos.x + ", " + pos.y);
-        console.log(evt);
+        thingX = pos.x;
+        thingY = pos.y;
     }, false);
 
-//$('#screenCanvas').on 'pointermove', (evt) ->
-//evt.preventDefault()
-//return if window.pointerId != evt.pointerId
-//window.engine.mouseMove Math.floor(evt.clientX / window.scale), Math.floor(evt.clientY / window.scale)
-//
-//$('#screenCanvas').on 'pointerup', (evt) ->
-//evt.preventDefault()
-//return if window.pointerId != evt.pointerId
-//window.pointerId = null
-//window.engine.mouseUp Math.floor(evt.clientX / window.scale), Math.floor(evt.clientY / window.scale)
+    document.addEventListener(startEventName, function(evt, elem) {
+        evt.preventDefault()
+        var pos = eventToMousePos(evt, elem);
+        thingX = pos.x;
+        thingY = pos.y;
+    }, false);
 
+    setTimeout(initGraphics, 0);
+}
+
+function onPause() {
+    console.log("onPause");
+    gPaused = true;
+}
+
+function onResume() {
+    console.log("onResume");
+    gPaused = false;
+    requestAnimationFrame(render);
+}
+
+function render(){
+    if (gPaused === true) {
+        return;
+    }
+
+    gContext.clearRect(0, 0, gWidth, gHeight);
+    gContext.setTransform(1, 0, 0, 1, 0, 0);
+    gContext.translate(thingX, thingY);
+    gContext.rotate(Math.PI / 2);
+    gContext.drawImage(thingImage, 0, 0);
+    FastCanvas.render();
+
+    requestAnimationFrame(render);
 }
 
 var app = {
@@ -110,7 +118,14 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        var isPhone = !!window.cordova;
+        if(isPhone) {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+            document.addEventListener("pause", onPause, false);
+            document.addEventListener("resume", onResume, false);
+        } else {
+            this.onDeviceReady();
+        }
     },
     // deviceready Event Handler
     //
